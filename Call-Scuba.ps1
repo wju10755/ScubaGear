f8e17133ddc37b52f50314f71c66152f8b12f248
MITS <Billu@advancestuff.com>
2024-01-03 21:19:55 -0500

    Updated output

--- Call-Scuba.ps1
+++ Call-Scuba.ps1
@@ -1,12 +1,12 @@
 Write-Host -ForegroundColor Yellow "Loading CISA SCuBA Baseline Report Tool..."
 $scubaDir = "C:\temp\scuba"
 if (!(Test-Path $scubaDir)){
-    New-Item -ItemType Directory -Path "c:\temp\scuba"
+    New-Item -ItemType Directory -Path "c:\temp\scuba" | Out-Null
 } else {
     Write-Host "Scuba directory already exists."
 }
 
 Invoke-WebRequest -uri "https://raw.githubusercontent.com/wju10755/ScubaGear/main/Start-Scuba.ps1" -OutFile "C:\temp\scuba\Start-Scuba.ps1"
 $Scuba = "c:\temp\scuba\Start-Scuba.ps1"
-$Process = Start-Process -FilePath powershell $Scuba -Wait
-Remove-item -path "C:\temp\scuba\Start-Scuba.ps1"
+Start-Sleep -Seconds 3
+ Remove-item -path "C:\temp\scuba\Start-Scuba.ps1"
