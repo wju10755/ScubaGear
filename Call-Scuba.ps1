4751d76b9f7c3130842c2755d2ecc4ddca9844dc
MITS <Billu@advancestuff.com>
2024-01-04 09:47:12 -0500

    Updated

--- Call-Scuba.ps1
+++ Call-Scuba.ps1
@@ -8,7 +8,7 @@ if (!(Test-Path $scubaDir)){
     Write-Host "Scuba directory already exists."
 }
 
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/wju10755/ScubaGear/main/Start-Scuba.ps1" -OutFile "C:\temp\scuba\Start-Scuba.ps1"
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/wju10755/ScubaGear/main/Start-Scuba-v2.ps1" -OutFile "C:\temp\scuba\Start-Scuba.ps1"
 $Scuba = "c:\temp\scuba\Start-Scuba.ps1"
 $Process = Start-Process -FilePath powershell $Scuba -Wait
 Start-Sleep -Seconds 3
